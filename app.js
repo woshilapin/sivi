ce8f0672e59b8d874a90d3fbc45b700a4e9da6e3
Jean SIMARD <woshilapin@gmail.com>
2014-08-22 09:05:23 +0200

    Move to LESS technology for stylesheets

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
