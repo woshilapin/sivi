61db9a0a51e4a5f6441aec20eb758c13c66505e8
Jean SIMARD <woshilapin@gmail.com>
2014-08-26 11:14:28 +0200

    Add a button to load the CV

--- public/javascripts/angular/cvController.js
+++ public/javascripts/angular/cvController.js
@@ -1,28 +1,36 @@
 var cvApp = angular.module('cvApp', []);
 
 cvApp.controller('cvController', ['$scope', '$http', function($scope, $http) {
+		var cv = undefined;
 		$scope.email= "simard.jean@gmail.com";
 		$scope.check = {
-			"email": /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 			"address": {
 				"postal": /^[0-9A-Z -]+$/
 			}
 		};
+		$scope.isCV = false;
 		$scope.checkCV = function() {
 			var email = $scope.email;
+			$scope.isCV = false;
 			if(email !== "") {
 				var uri = 'cv/' + email + '/last';
 				$http.get(uri).
 				success(function(data, status, header, config) {
 					if(status === 200) {
-						$scope.cv = data;
+						$scope.isCV = true;
+						cv = data;
 						// Need to explicitly convert date into Date object in javascript
-						$scope.cv.birthday = new Date(data.birthday);
+						cv.birthday = new Date(data.birthday);
 					}
 				}).
 				error(function(data, status, header, config) {
 					console.log('The database does not contain any CV for user \`' + email + "'");
 				});
+			}
+		};
+		$scope.loadCV = function() {
+			if(cv) {
+				$scope.cv = cv;
 			}
 		};
 }]);
